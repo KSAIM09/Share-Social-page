a8ae5944777d6a30a7bdf426d3d278b20607a839
Saim Khan <93315183+KSAIM09@users.noreply.github.com>
2024-06-08 04:41:19 +0000

    Final Completed

--- social_sharePage/src/SharePage.jsx
+++ social_sharePage/src/SharePage.jsx
@@ -28,7 +28,71 @@ function SharePage () {
                         </div>
                     </div>
                     <div className="right">
-                        <div className="r-con"></div>
+                        <div className="r-con">
+                            <div className="right-btn-top">
+                                <button id='btn1'>ADD TO CART</button>
+                                <button id='btn2'>ADD TO WISHLIST</button>
+                            </div>
+                            <div className='box'>
+                                <p className='left-side'>Epic Rewards</p>
+                                <p className='right-side'>Earn 5% Back</p>
+                            </div>
+
+                            <div className='box'>
+                                <p className='left-side'>Refund Type</p>
+                                <p className='right-side'>Refundable</p>
+                            </div>
+
+                            <div className='box'>
+                                <p className='left-side'>Developer</p>
+                                <p className='right-side'>Criterion Games</p>
+                            </div>
+
+                            <div className='box'>
+                                <p className='left-side'>Publisher</p>
+                                <p className='right-side'>Electronic Arts</p>
+                            </div>
+
+                            <div className='box'>
+                                <p className='left-side'>Release Date</p>
+                                <p className='right-side'>11/29/22</p>
+                            </div>
+
+                            <div className='box'>
+                                <p className='left-side'>Initial Release</p>
+                                <p className='right-side'>12/02/22</p>
+                            </div>
+
+                            <a href="#">See All Editions and Add-Ons</a>
+
+                            <div className="bottom-btn">
+                                <button id='sharebtn'>SHARE</button>
+                                <button>REPORT</button>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="share-box">
+                    <div className="share-con">
+                        <span><i class="ri-close-large-line"></i></span>
+                        <div className='middle'>
+                            <h1>Share Game</h1>
+                            <p>Select a Social Network to Share to.</p>
+                            <div className='icons'>
+                                <span><i class="ri-facebook-circle-fill"></i></span>
+                                <span><i class="ri-twitter-fill"></i></span>
+                                <span><i class="ri-discord-fill"></i></span>
+                            </div>
+
+                        </div>
+
+                        <div className="share-link">
+                            <p>https://store.epicgames.com/en-US/p/need-f...</p>
+                            <span id='copy'><i class="ri-file-copy-line"></i></span>
+                        </div>
                     </div>
                 </div>
             </div>
